ab6587f8618cfa6bd0f4bc9e4fa9c5de3cdf1380
Angelo <angelo.01@gmx.ch>
2019-07-01 22:02:06 +0200

    Tried to do some Stuff with field gui

--- src/ch/bzz/Uno/GUI/ViewController/SpielbrettController.java
+++ src/ch/bzz/Uno/GUI/ViewController/SpielbrettController.java
@@ -1,7 +1,8 @@
-package ch.bzz.Uno.GUI.ViewController;
+package ch.bzz.Uno.GUI.viewController;
 
 import ch.bzz.Uno.Interfaces.ControllerInterface;
 import ch.bzz.Uno.model.Card;
+import ch.bzz.Uno.model.Player;
 
 public class SpielbrettController implements ControllerInterface {
     @Override
@@ -32,5 +33,9 @@ public class SpielbrettController implements ControllerInterface {
     @Override
     public void nextHasBeenPressed() {
 
+    }
+
+    public Player getPlayer(){
+        return controller.getCurrentlyPlaying();
     }
 }
